ba5bb280e34702647a55ae7625d0df7acdcf83e9
hillebrand <hillebrand@eleven.nl>
2016-03-13 20:30:28 +0100

    Report through slack

--- src/main/kotlin/nl/hillebrand/ScheduledChecker.kt
+++ src/main/kotlin/nl/hillebrand/ScheduledChecker.kt
@@ -3,32 +3,55 @@ package nl.hillebrand
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+import java.util.*
 
 @Component
 class ScheduledChecker {
 
-    val baseUri = "https://boeken.rederij-doeksen.nl/products/departures?booking[departure_port]=H" +
+    val boatScheduleEndpoint = "https://boeken.rederij-doeksen.nl/products/departures?booking[departure_port]=H" +
             "&booking[arrival_port]=T&booking[departure_date]=23-07-2016&booking[party_type]=normal" +
             "&booking[pax][V]=0&booking[pax][V65]=2&booking[pax][K]=0&booking[pax][K4]=0&booking[pax][ip]=0" +
             "&booking[options][AL]=1&booking[specifications][AL]=FR&booking[retour_journey]=1" +
             "&booking[return_date]=13-08-2016&booking[ticket_type]=twoway&booking[only_available]=0"
 
+    val slackEndpoint = "https://hooks.slack.com/services/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"
+
     val restTemplate = RestTemplate()
 
-    @Scheduled(fixedRate = 10000)
+//    @Scheduled(fixedRate = 10000)
+    @Scheduled(cron="0 9 * * * *")
     fun check() {
-        val timeTable = restTemplate.getForObject(baseUri, TimeTable::class.java);
-        println("---------------------------------------------------------------------")
+        val timeTable = restTemplate.getForObject(boatScheduleEndpoint, TimeTable::class.java);
 
-        for (passage in timeTable.outwards) {
-            val maybe = if (passage.available) " " else " not "
-            println("Outward passage on ${passage.departureTime} is${maybe}available ")
-        }
-        println("---------------------------------------------------------------------")
-        for (passage in timeTable.retour) {
-            val maybe = if (passage.available) " " else " not "
-            println("Retour passage on ${passage.departureTime} is${maybe}available")
-        }
-        println("---------------------------------------------------------------------")
+
+        val outPassage = Attachment(
+                "Heenreis",
+                arrayListOf(Field(
+                        "Tijd",
+                        timeTable.outwards.map{ passage -> passage.departureTime }.joinToString("\n")
+                ), Field(
+                        "Beschikbaar",
+                        timeTable.outwards.map{ passage -> passage.available }.joinToString("\n")
+                )),
+                "danger"
+        )
+        val retourPassage = Attachment(
+                "Terugreis",
+                arrayListOf(Field(
+                        "Tijd",
+                        timeTable.retour.map{ passage -> passage.departureTime }.joinToString("\n")
+                ), Field(
+                        "Beschikbaar",
+                        timeTable.retour.map{ passage -> passage.available }.joinToString("\n")
+                )),
+                "danger"
+        )
+
+        val message = Message(
+                "Beschikbaarheid op %td/%tm/%ty".format(Date(), Date(), Date()),
+                arrayListOf(outPassage, retourPassage)
+        )
+
+        restTemplate.postForLocation(slackEndpoint, message)
     }
 }
