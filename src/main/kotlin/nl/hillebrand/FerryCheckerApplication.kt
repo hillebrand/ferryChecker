b5f5012b573e14d0138c0f3d064d9fb0dfe076fa
hillebrand <hillebrand@eleven.nl>
2016-03-14 15:38:16 +0100

    Auto config annotation

--- src/main/kotlin/nl/hillebrand/FerryCheckerApplication.kt
+++ src/main/kotlin/nl/hillebrand/FerryCheckerApplication.kt
@@ -1,11 +1,13 @@
 package nl.hillebrand
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration
 open class FerryCheckerApplication {
     companion object {
 
